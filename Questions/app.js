8fa74becacab30b42ee2e6c952b34ff755390b6c
Shivam <shivam221294@gmail.com>
2021-06-18 21:33:06 +0530

    first commit into Questions

---
+++ Questions/app.js
@@ -0,0 +1,21 @@
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+console.log(questions);
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+        console.log(item);
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
